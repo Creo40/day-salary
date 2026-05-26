dd3e6ecfbcaa46551de581f381f6c2d71e03f4c9
Carol <1287161926@qq.com>
2026-05-26 11:03:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-var C='dayw-v1';
-var A=['./index.html','./manifest.json','./script.js'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(A)}))});
+var C='dayw-v2';
+self.addEventListener('install',function(e){e.waitUntil(caches.open(C).then(function(c){return c.addAll(['./index.html','./script.js','./manifest.json'])})});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
